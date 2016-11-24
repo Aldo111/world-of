6df5f2867f2d543046919228ad297cdc8bcb73a7
ADARSH SINHA <adarsh111@hotmail.com>
2016-11-24 18:48:12 -0500

    Fixed share UI

--- js/controllers/dialogs/ShareButton.js
+++ js/controllers/dialogs/ShareButton.js
@@ -1,6 +1,10 @@
 app.controller('ShareButton', function($scope, $state, API, User, Loader,
   $mdDialog) {
-  this.sites = [{"name":"twitter","img":"img/twitter-128.png"}, {"name":"facebook","img":"img/facebook.png"},{"name":"reddit","img":"img/reddit.png"}];
+  this.worldUrl = document.URL.replace('dash', 'worlds/' + this.worldId);
+  this.sites = [
+    {"name":"twitter","img":"img/twitter-128.png"},
+    {"name":"facebook","img":"img/facebook.png"},
+    {"name":"reddit","img":"img/reddit.png"}];
   this.cancel = function(data) {
     $mdDialog.hide(data);
   };
